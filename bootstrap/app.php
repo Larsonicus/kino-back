f02ab4f2a48a55f9db5b2589d7131f7ecb14eea5
Pavel Fedorin <fedorin.pasha59@gmail.com>
2025-10-23 08:41:51 +0300

    feat: handle exceptions as json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         api: __DIR__.'/../routes/api.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            return true;
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+                return response()->json(['message' => 'Not Found!'], 404);
+        });
     })->create();
